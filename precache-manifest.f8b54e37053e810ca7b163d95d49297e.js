ad527702fc249c49c59439ee29f356592c55285f
Oleg Liepkalo <liepkalo.o@gmail.com>
2019-12-29 23:04:18 +0200

    Updates

---
+++ precache-manifest.f8b54e37053e810ca7b163d95d49297e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2fd045e4c71970681903ceeef2c62e",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "ba2914f5531b7aee5976",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.33bd9b1c.chunk.css"
+  },
+  {
+    "revision": "8b234865927569681001",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.97de1465.chunk.js"
+  },
+  {
+    "revision": "ba2914f5531b7aee5976",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.37dce27b.chunk.js"
+  },
+  {
+    "revision": "dffe8cd1e2fc699743fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.d2e8316a.js"
+  }
+]);
